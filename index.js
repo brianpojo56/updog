77f94799c8097d8ff73946add22f57e0e1018ea6
Brian Ellingson <brianpojo56@gmail.com>
2020-04-23 23:14:42 -0500

    Additional test cases, and added eslint for format and style checks

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function() {
-    return 'updog'
+/**
+ * Updog module.
+ * @module updog
+ */
+
+/**
+ * Generates string literal representation of updog.
+ * @param {Object} [configuration] - Optional configuration mapping for updog
+ * @return {string} String literal representation of updog
+ */
+module.exports = function () {
+  return 'updog'
 }
--- index.test.js
+++ index.test.js
@@ -1,33 +1,45 @@
-const updog = require('./index.js');
+const updog = require('./index.js')
 
 test('Test truthiness of updog', () => {
-  expect(updog()).toBeTruthy();
-});
+  expect(updog()).toBeTruthy()
+})
 
 test('Test that updog is not divisible', () => {
-  expect(updog() / 1).toBeNaN();
-});
+  expect(updog() / 1).toBeNaN()
+})
 
-test('Test updog length is big enough', () => {
-  expect(updog().length).toBeGreaterThan(4);
-});
+test('Test that updog is not multipliable', () => {
+  expect(updog() * 1).toBeNaN()
+})
 
-test('Test updog length not too big', () => {
-  expect(updog().length).toBeLessThan(6);
-});
+test('Test that updog is not a number', () => {
+  expect(Number(updog())).toBeNaN()
+})
 
-test('Test updog length is just right', () => {
-  expect(updog().length).toBe(5);
-});
+test('Test that updog length is big enough', () => {
+  expect(updog().length).toBeGreaterThan(4)
+})
 
-test('Test updog value is correct', () => {
-  expect(updog()).toEqual('updog');
-});
+test('Test that updog length not too big', () => {
+  expect(updog().length).toBeLessThan(6)
+})
 
-test('Test updog value is absolutely correct', () => {
-  expect(updog()).toStrictEqual('updog');
-});
+test('Test that updog length is just right', () => {
+  expect(updog().length).toBe(5)
+})
 
-test('Test that updog is Stringable', () => {
-  expect(new String(updog())).toBeInstanceOf(String);
-});
+test('Test that updog value is correct', () => {
+  expect(updog()).toEqual('updog')
+})
+
+test('Test that updog value is absolutely correct', () => {
+  expect(updog()).toStrictEqual('updog')
+})
+
+test('Test that updog is not an Array', () => {
+  expect(updog()).not.toBeInstanceOf(Array)
+})
+
+test('Test that updog is String', () => {
+  expect(typeof updog()).toStrictEqual('string')
+})
